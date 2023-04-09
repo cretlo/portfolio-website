2e0c07c574981311bf713da390faf93ff500f89a
Ian <ian.reimers.apps@gmail.com>
2023-04-09 05:19:07 -0600

    added custom scss styles and changed design

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
-import './styles.css';
+import './main.scss';
+import AOS from 'aos/dist/aos.js';
+import 'aos/dist/aos.css';
+//import './styles.css';
 import jsTypeText from 'js-type-text';
+
+
+AOS.init({
+    offset: 250,
+    duration: 700,
+    delay: 0,
+    easing: 'ease',
+    once: false,
+    mirror: false,
+    anchorPlacement: 'top-bottom'
+});
 
 
 jsTypeText.start({
@@ -8,7 +22,7 @@ jsTypeText.start({
     speed: 110,
     cursor: false
 }, (result: string) => {
-    let element = document.querySelector(".masthead-subheading");
+    let element = document.querySelector(".text-gen");
     if (!element) return;
     if (element?.classList.contains("invisible")) element.classList.remove("invisible")
     element.innerHTML = result;
