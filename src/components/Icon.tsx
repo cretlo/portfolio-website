a9db180ad96478fd88626eec77d06509389bb8f1
Ian Reimers <ian.reimers.apps@gmail.com>
2024-09-04 22:52:50 -0600

    feat: add certificates section with CompTIA certificate logos

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -39,6 +39,8 @@ import {
 import { PiFileSqlFill } from "react-icons/pi";
 import { DiJava } from "react-icons/di";
 import { TbArrowBadgeDown } from "react-icons/tb";
+import SecurityPlus from "../assets/logos/securityplus-logo-certified-ce-black.png";
+import NetworkPlus from "../assets/logos/networkplus-logo-certified-ce-black.png";
 
 const iconMap = {
     javascript: SiJavascript,
@@ -78,15 +80,38 @@ const iconMap = {
     github: SiGithub,
     git: SiGit,
     downArrow: TbArrowBadgeDown,
-    springboot: SiSpringboot
+    springboot: SiSpringboot,
+    securityplus: SecurityPlus,
+    networkplus: NetworkPlus,
 };
 
 export default function Icon({
     title,
-    size = 48,
-    className = "fill-slate-500 hover:fill-teal-700",
+    size = 58,
+    className = "fill-slate-500",
+}: {
+    title: string;
+    size: number | undefined;
+    className: string | undefined;
 }) {
     const IconComponent = iconMap[title];
+
+    // Edge case since CompTIA doesn't provide SVG logo files
+    //
+    // The filter was calculated from https://codepen.io/sosuke/pen/Pjoqqp with the slate-500 color extracted using
+    // firefox dev tools color picker
+    if (title.includes("plus")) {
+        return (
+            <img
+                src={IconComponent.src}
+                alt={title + " logo"}
+                style={{
+                    filter: "invert(42%) sepia(37%) saturate(256%) hue-rotate(176deg) brightness(97%) contrast(92%);",
+                    width: size * 1.5,
+                }}
+            />
+        );
+    }
 
     return <IconComponent title={title} size={size} className={className} />;
 }
