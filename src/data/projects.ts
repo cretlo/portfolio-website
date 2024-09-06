36007e557a9620ef43a19d0ff22059ade97a4c3c
Ian Reimers <ian.reimers.apps@gmail.com>
2024-09-05 23:03:15 -0600

    fix: update github links

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -13,10 +13,17 @@ export default [
             title: "Pomodoro Tracker",
             imgData: pomodoroImgData,
             desc: "Screenshot of Pomodoro Tracker timer and dashboard analytics.",
-            technologies: ["nextjs", "react", "tailwind", "java", "springboot", "postgres"],
+            technologies: [
+                "nextjs",
+                "react",
+                "tailwind",
+                "java",
+                "springboot",
+                "postgres",
+            ],
             about: `Implementing the concept of the Pomodoro Technique, the fullstack application can be used as a timer for your task and break sessions. With an account made, and logged in, the app tracks your task, short break, and long break times to provide you with analytics of your pomodoro sessions, located in your personal dashboard. The session timer is completely configurable through the settings tab for logged in and non-logged in usage, and provides a list of timer sounds when completing/transitioning into a new session.`,
             link: "https://pomodoro.ianreimers.com/",
-            github: "https://github.com/cretlo/pomodoro-tracker",
+            github: "https://github.com/ianreimers/pomodoro-tracker",
         },
     },
     {
@@ -39,7 +46,7 @@ export default [
             ],
             about: `The app is a personal planner that allows you persist all your events, to-dos, and notes with a personal account. The goal was to build the majority of the functionality, using the native APIs from the technologies above, completely in Typescript. The reason was to feel the pain points of building your own authentication, state management, and REST APIs before utilizing libraries and frameworks that abstract a lot of the bare logic from you. This allowed me to gain deeper insight into what goes into making a strictly typed, fullstack application, and when to reach for libraries that can resolve those pain points. However, DrizzleORM was used for the types and database synchronization. Additionally, learning a drastic amount of docker through the process, the app is completely dockerized for easier development/deployment, and hosted on a VPS using Nginx as a reverse proxy to understand the DIY deployment process.`,
             link: "https://planner.ianreimers.com",
-            github: "https://github.com/cretlo/fullstack-personal-dashboard",
+            github: "https://github.com/ianreimers/fullstack-planner-app",
         },
     },
     {
@@ -53,7 +60,7 @@ export default [
             technologies: ["astro", "bootstrap", "sass"],
             about: `The single page website is built for an assisted living home located in Erie, CO. Originally built with Webpack and Sass, I recently ported the site over to Astro, for a better developer experience, and Bootstrap customized with Sass. With the clients request to keep it simple, accessible, fast, and easy to navigate, the website currently possesses a Google Lighthouse score of 99 performance, 100 accessibility, 100 best practices, and 100 SEO. On the quest for maximum optimizations, I learned how to implement responsive images for better loading times, since the gallery section contained many of them.`,
             link: "https://charlestonal.com",
-            github: "https://github.com/cretlo/charlestonal",
+            github: "https://github.com/ianreimers/charlestonal",
         },
     },
     {
@@ -67,7 +74,7 @@ export default [
             technologies: ["nextjs", "tailwind", "express", "stripe"],
             about: `Mom Club is a non-profit organization located in Laramie, Wyoming. Originally built using create-react-app and Sass, I ported the project to NextJs and Tailwind since the client wants to implement more robust functionality in the near future. This was also an opportunity to learn Tailwind, which I thoroughly enjoyed. Stripe was used for handling the donation transactions and modified using NodeJs since donations were not a default feature of the Stripe API.`,
             link: "https://momclubwyo.org",
-            github: "https://github.com/cretlo/momclub",
+            github: "https://github.com/ianreimers/momclub",
         },
     },
     {
@@ -81,7 +88,7 @@ export default [
             technologies: ["csharp", "unity", "firebase", "blender"],
             about: `The end goal of the project was to build a time-based virtual reality destruction sandbox, taking motivation from the 1990s 2D game, Rampage. Completely physics based, the buildings are entirely destructible, allowing the player to cause destruction anyway they see fit. The player comes equipped with various abilities and movement capabilities such as walking, turning, leaping, climbing, punching, grabbing, and throwing, allowing for a dynamic motion experience. Additionally, for the time-based aspect of the experience, the player is timed with a tracked score that equates to the destruction they’ve caused, saved to a Firebase database, and displayed on an in-game world leaderboard. All of this resides in an environment completely constructed from Blender and native Unity components.`,
             link: "",
-            github: "https://github.com/cretlo/unity-vr-rampage/blob/main/notebook/final.md",
+            github: "https://github.com/ianreimers/unity-vr-rampage/blob/main/notebook/final.md",
         },
     },
 ];
